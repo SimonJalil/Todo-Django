d97f573dc6957540540f92172b2722e7a520b3b6
Simon Jalil <jalileliassimon.cruz@veladero.com>
2025-09-24 10:16:59 -0300

    Creacion de funcion logout

--- todo_django/urls.py
+++ todo_django/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("", views.home, name="hello"),
     path("signup/", views.signup, name="signup"),
     path("tasks/", views.tasks, name="tasks"),
+    path("logout/", views.signout, name="logout"),
 ]
