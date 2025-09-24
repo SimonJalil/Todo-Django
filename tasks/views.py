8ae244e75ca4cbf8d22a41c04c839cf3f35d57e8
Simon Jalil <jalileliassimon.cruz@veladero.com>
2025-09-24 10:40:25 -0300

    Feature login terminado

--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 
@@ -26,7 +26,7 @@ def signup(request):
                 user.save()
                 login(request, user)
                 return redirect('tasks')
-            
+
             except IntegrityError:
                 context = {
                     'form': UserCreationForm(),
@@ -34,17 +34,40 @@ def signup(request):
                 }
 
                 return render(request, 'signup.html', context)
-        
+
         context = {
             'form': UserCreationForm(),
             'error': "Passwords do not match"
-        }   
+        }
 
         return render(request, 'signup.html', context)
+
 
 def tasks(request):
     return render(request, 'tasks.html')
 
+
 def signout(request):
     logout(request)
     return redirect('home')
+
+
+def signin(request):
+    if request.method == 'GET':
+        context = {
+            'form': AuthenticationForm()
+        }
+
+        return render(request, 'signin.html', context)
+    else:
+        user = authenticate(request, username = request.POST['username'], password = request.POST['password'])
+
+        if user is None:
+            context = {
+                'form': AuthenticationForm(), 
+                'error': "Username or password is incorrect"
+            }
+            return render(request, 'signin.html', context)
+        else:
+            login(request, user)
+            return redirect('tasks')
