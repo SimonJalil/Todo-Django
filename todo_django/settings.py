d342f8cc63db212c75649052af29255f456fd73c
Simón Elías Jalil Cruz <simonjalilcruz@gmail.com>
2025-10-02 20:23:31 -0300

    Configuracion previa a Deploy

--- todo_django/settings.py
+++ todo_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-y1u0*-0s&zp6i1pmvnbv2a@dy#&a5fqbe@@_k@waukh&t8&jj%"
+# SECRET_KEY = "django-insecure-y1u0*-0s&zp6i1pmvnbv2a@dy#&a5fqbe@@_k@waukh&t8&jj%"
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['simon2324.pythonanywhere.com']
 
 
 # Application definition
@@ -171,4 +173,10 @@ ADMIN_LOGIN_URL = '/admin/login/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = BASE_DIR / 'static'
+try:
+    from .settings_dev import *
+except ModuleNotFoundError:
+    pass
 
